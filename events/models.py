146410f5fcde15a8b2144773134953f67a336227
Mati <272225@student.pwr.edu.pl>
2024-12-23 15:58:27 +0100

    Basic frontend

--- events/models.py
+++ events/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)  
     name = models.CharField(max_length=100)  
-
+    
     def __str__(self):
         return self.name 
 
