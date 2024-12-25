cf848968d0dfe2406088c3ee223b8a641a56f61a
Matuesz Logon <mateusz.logon@onet.pl>
2024-12-25 17:30:44 +0100

    Connecting frontend to database

--- events/urls.py
+++ events/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "events"
 urlpatterns = [
     path("", views.index, name="index"),
+    path('send_user_data/', views.send_user_data, name='send_user_data'),
     
 ]
