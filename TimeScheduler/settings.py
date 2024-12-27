1a8496a9ff6285aafb434741a3708eeefded885d
Matuesz Logon <mateusz.logon@onet.pl>
2024-12-27 19:41:39 +0100

    Creating Separated Front-end

--- TimeScheduler/settings.py
+++ TimeScheduler/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -71,6 +74,11 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'TimeScheduler.wsgi.application'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
 
 
 # Database
--- events/views.py
+++ events/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-def index(request):
-    return render(request, 'events/index.html')  
 
-def send_user_data(request):
+@api_view(['POST'])
+def create_user(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        
-        if name and email:
-            try:
-                user = User.objects.create(name=name, email=email)
-                return JsonResponse({'name': user.name, 'email': user.email})
-            
-            except Exception as e:
-                return JsonResponse({'error': f'Error saving data: {str(e)}'}, status=400)
-
-        return JsonResponse({'error': 'Invalid data'}, status=400)
-    
-    return JsonResponse({'error': 'Invalid request method'}, status=405)
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  
+            return Response({'message': 'User created successfully!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
